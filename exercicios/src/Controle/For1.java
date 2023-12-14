bc375508ff4e6d73e5974030ada27b46442a35f3
JOSE AUGUSTO MARTINS QUEIROZ <joseaugustomartinsqueiroz@gmail.com>
2023-12-14 20:30:16 -0300

    Create For1.java

---
+++ exercicios/src/Controle/For1.java
@@ -0,0 +1,31 @@
+package Controle;
+
+public class For1 {
+
+	public static void main(String[] args) {
+
+//		int contador = 1;
+//
+//		while (contador <= 10) {
+//			System.out.printf("i = %d\n", contador);
+//			contador++;
+//		}
+		
+		for(int contador = 1; contador <= 10; contador ++) {
+			System.out.printf("i = %d\n", contador);
+		}
+		
+		// Sem todos os paramentros!
+//		int x = 2;
+//		for(; x < 10;) {
+//			System.out.println("\nX = " + x);
+//			x++;
+//		}
+		
+		// Laço infinito!!
+//		for(;;) {
+//			System.out.println("Fim!!");
+//		}
+	
+	}
+}
